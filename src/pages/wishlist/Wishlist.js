97cd1e75a32d616c6097da7b3528a093e4dbe1ff
Nikhil5705 <vermanikhil5705@gmail.com>
2023-08-16 02:14:37 +0530

    fix: add to cart btn

--- src/pages/wishlist/Wishlist.js
+++ src/pages/wishlist/Wishlist.js
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router';
 
 export const Wishlist = () => {
 const { wishlistItem, removeFromWishlistHandler } = useContext(WishlistContext);
-const {cartHandler} = useContext(CartContext);
+const {cartItem, cartHandler} = useContext(CartContext);
 const navigate = useNavigate();
 
 const removeHandler=(item)=>{
@@ -18,7 +18,7 @@ const removeHandler=(item)=>{
       {wishlistItem.length === 0 && <h1>Look like you have not added anything in your wishlist. Why don't you buy something!!!</h1>}
       {wishlistItem.map((item) =>{
       const {_id, title, price, rating, type, image} = item;
-
+      const isItInCart = cartItem.find(prod => prod._id === _id)
       return (
         <div key={_id} className='product_card_wishlist'>
           <img className='prod_image_wishlist' src={image} alt={title}/>
@@ -29,13 +29,14 @@ const removeHandler=(item)=>{
             <div className='prod_rating_wishlist'>rating: {rating}</div>
           </div>
           <div className='actions_wishlist'>
-            <button className='add_to_cart_from_wishlist' onClick={() =>{cartHandler(item)}}>Add To Cart</button>
+          {isItInCart ? <button className='add_to_cart_from_wishlist' onClick={() => {navigate(`/cart`);}}>Go To Cart</button> : <button className='add_to_cart_from_wishlist' onClick={() =>{cartHandler(item)}}>Add To Cart</button>}
             <button className='remove_from_wishlist' onClick={()=>removeHandler(item)}>Remove</button>
           </div>
         </div>
       )}) 
       } 
-      {wishlistItem.length != 0 ? <button onClick={() => navigate("/checkout")}>Checkout</button> : <button className='add_to_wishlist' onClick={() =>navigate("/product")}>SHOP NOW</button>}
+      
+      {wishlistItem.length === 0 && <button className='add_to_wishlist' onClick={() =>navigate("/product")}>SHOP NOW</button>}
     </div>
   )
 }
